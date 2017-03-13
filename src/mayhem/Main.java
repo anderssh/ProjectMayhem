e1199e9ad3f033e52e96827fc36550a4add9170f
Vetle Fredriksen <Vetle Fredriksen>
2017-03-13 17:43:00 +0100

    menu

--- src/mayhem/Main.java
+++ src/mayhem/Main.java
@@ -7,33 +7,46 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Velg handling:");
-		System.out.println("Registrer trening [1]");
-		System.out.println("Se tidligere trening [2]");
-		System.out.println("Statistikk [3]");
-		System.out.println("Notater [4]");
-		System.out.println("Se tidligere trening [5]");
-		
-		Scanner in = new Scanner(System.in);
-		int i = in.nextInt();
-		switch(i){
-			case 1:
-					System.out.println("jjaja");
+		boolean done = false;
+		while(!done){
+			System.out.println("Velg handling:");
+			System.out.println("[1] Registrer trening");
+			System.out.println("[2] Se tidligere trening");
+			System.out.println("[3] Statistikk");
+			System.out.println("[4] Notater");
+			System.out.println("[5] Se tidligere trening");
+			System.out.println("[6] Avslutt");
+			Scanner in = new Scanner(System.in);
+			int i = in.nextInt();
+			switch(i){
+				case 1:
+						System.out.println("jjaja");
+					
+						break;
+				case 2: 
+						System.out.println("asasas");
+						break;
+				case 3: 
+						break;
+				case 4:
+						break;
+				case 5: 
+						break;
+				case 6: done = true;
+						break;
+				default: 
+					System.out.println("Skriv inn tall fra 1-6");
 					break;
-			case 2: System.out.println("asasas");
-					break;
-			case 3: 
+			}
 		}
-		
+		System.out.print("Takk for idag");
 	/*	MySQLAccess dao = new MySQLAccess();
 		dao.readDataBase();
 		System.out.println("sdf");
 		PropertyHandling jee = new PropertyHandling();
 <<<<<<< HEAD
 		//jee.CreateDatabaseProperties();
-		jee.LoadDatabaseProperies();*/
-=======
-		jee.CreateDatabaseProperties();
->>>>>>> 5e3cd95847e3f9fdefc50d159dbb897d92eecebb
+		jee.LoadDatabaseProperies();
+		jee.CreateDatabaseProperties();*/
 	}
 }
