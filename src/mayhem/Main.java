e2de7c1d5a173f7ff75fa4fdb2d22ec0767e2935
anderssh <anders.s.helmen@gmail.com>
2017-03-13 13:20:36 +0100

    Added Propertyhandling

--- src/mayhem/Main.java
+++ src/mayhem/Main.java
@@ -1,10 +1,13 @@
 package mayhem;
 
+import mayhem.PropertyHandling;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("sdf");
+		PropertyHandling jee = new PropertyHandling();	
 	}
 
 }
---
+++ src/mayhem/PropertyHandling.java
@@ -0,0 +1,65 @@
+package mayhem;
+
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class PropertyHandling {
+	
+	public void CreateDatabaseProperies () {
+		Properties prop = new Properties();
+    	OutputStream output = null;
+    	try {
+
+    		output = new FileOutputStream("config.properties");
+
+    		// set the properties value
+    		prop.setProperty("dbdriver", "javax.sql.DataSource");
+    		prop.setProperty("dbURL", "jdbc:mysql://localhost:3306/project2?autoReconnect=true&useSSL=false");
+    		prop.setProperty("dbuser", "rall");
+    		prop.setProperty("dbpassword", "rallstad");
+
+    		// save properties to project root folder
+    		prop.store(output, null);
+
+    	} catch (IOException io) {
+    		io.printStackTrace();
+    	} finally {
+    		if (output != null) {
+    			try {
+    				output.close();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+
+    	}
+	}
+	public Properties LoadDatabaseProperies () {
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return prop;
+	}
+}
