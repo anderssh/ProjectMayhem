44f473a4262ea84c75bff4d58b87ae19720555af
anderssh <anders.s.helmen@gmail.com>
2017-03-13 17:40:18 +0100

    Started on the registertraining

--- src/mayhem/Main.java
+++ src/mayhem/Main.java
@@ -3,37 +3,30 @@ package mayhem;
 import mayhem.PropertyHandling;
 import mayhem.MySQLAccess;
 import java.util.Scanner;
+import mayhem.RegisterTraining;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Velg handling:");
-		System.out.println("Registrer trening [1]");
-		System.out.println("Se tidligere trening [2]");
-		System.out.println("Statistikk [3]");
-		System.out.println("Notater [4]");
-		System.out.println("Se tidligere trening [5]");
+		
+		System.out.println("Velg handling: \n");
+		System.out.println("[1] \t Registrer trening");
+		System.out.println("[2] \t Se tidligere trening");
+		System.out.println("[3] \t Statistikk ");
+		System.out.println("[4] \t Notater");
+		System.out.println("[5] \t Se tidligere trening ");
 		
 		Scanner in = new Scanner(System.in);
 		int i = in.nextInt();
 		switch(i){
 			case 1:
 					System.out.println("jjaja");
+					RegisterTraining regTraining= new RegisterTraining();
+					regTraining.startRegister();
 					break;
 			case 2: System.out.println("asasas");
 					break;
 			case 3: 
 		}
-		
-	/*	MySQLAccess dao = new MySQLAccess();
-		dao.readDataBase();
-		System.out.println("sdf");
-		PropertyHandling jee = new PropertyHandling();
-<<<<<<< HEAD
-		//jee.CreateDatabaseProperties();
-		jee.LoadDatabaseProperies();*/
-=======
-		jee.CreateDatabaseProperties();
->>>>>>> 5e3cd95847e3f9fdefc50d159dbb897d92eecebb
 	}
 }
