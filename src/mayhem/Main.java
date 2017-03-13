886e95a4d22918ba734fc8c1d894d3547db97210
Vetle Fredriksen <Vetle Fredriksen>
2017-03-13 16:43:32 +0100

    begynt på menu

--- src/mayhem/Main.java
+++ src/mayhem/Main.java
@@ -2,16 +2,34 @@ package mayhem;
 
 import mayhem.PropertyHandling;
 import mayhem.MySQLAccess;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		MySQLAccess dao = new MySQLAccess();
+		System.out.println("Velg handling:");
+		System.out.println("Registrer trening [1]");
+		System.out.println("Se tidligere trening [2]");
+		System.out.println("Statistikk [3]");
+		System.out.println("Notater [4]");
+		System.out.println("Se tidligere trening [5]");
+		
+		Scanner in = new Scanner(System.in);
+		int i = in.nextInt();
+		switch(i){
+			case 1:
+					System.out.println("jjaja");
+					break;
+			case 2: System.out.println("asasas");
+					break;
+			case 3: 
+		}
+		
+	/*	MySQLAccess dao = new MySQLAccess();
 		dao.readDataBase();
 		System.out.println("sdf");
 		PropertyHandling jee = new PropertyHandling();
 		//jee.CreateDatabaseProperties();
-		jee.LoadDatabaseProperies();
+		jee.LoadDatabaseProperies();*/
 	}
 }
